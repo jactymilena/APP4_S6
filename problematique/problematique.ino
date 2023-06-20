75aabf2a6b5fc4234750cfc4b040142607d814b7
jactymilena <saej3101@usherbrooke.ca>
2023-06-20 15:34:27 -0400

    WIP Manchester decode

--- problematique/problematique.ino
+++ problematique/problematique.ino
@@ -8,16 +8,24 @@ const int RECEIVE_CORE = 0;
 const int SEND_CORE = 1;
 const int ZERO_MODE = 0;
 const int ONE_MODE = 1;
+const int HALF_PERIOD = 500;
+const int THRESHOLD_PERIOD = 5;
+
 
 std::vector<uint8_t> buffer;
 
 int valIn;
 int valOut;
+unsigned long duration;
 
+unsigned long timer;
+unsigned long lastChangeTime;
+bool checkPeriod;
 
 // Define functions
 void receiveRising();
-void receiveFalling();  
+// void receiveFalling();  
+void receivePulse(); 
 void TaskReceive(void *pvParameters);
 void TaskSend(void *pvParameters);
 
@@ -29,27 +37,29 @@ void setup() {
   Serial.begin(115200);
 
   // Receive interrupts
-  // attachInterrupt(digitalPinToInterrupt(PIN_IN), receiveRising, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(PIN_IN), receiveFalling, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_IN), receiveRising, RISING);
+  // attachInterrupt(digitalPinToInterrupt(PIN_IN), receivePulse, CHANGE);
 
   // Receive Task
   xTaskCreate(TaskReceive, "Receive Trame", 2048, NULL, 2,  NULL);
   // Send Task
   xTaskCreate(TaskSend, "Send Trame", 2048, NULL, 3,  NULL);
 
-
+  lastChangeTime = 0;
+  checkPeriod = true;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  unsigned long duration = pulseIn(PIN_IN, HIGH);
-  Serial.printf("pulse duration %d ", duration);
+  // duration = pulseIn(PIN_IN);
+  // Serial.printf("pulse duration %d ", duration);
+  timer = micros();
 
   valIn = digitalRead(PIN_IN); 
   valOut = digitalRead(PIN_OUT); 
 
-  Serial.printf("in %d ", valIn);
-  Serial.printf("ou %d\n", valOut);
+  // Serial.printf("in %d ", valIn);
+  // Serial.printf("ou %d\n", valOut);
 
 }
 
@@ -69,28 +79,28 @@ uint8_t* createTrame(uint8_t* data, uint8_t size) {
   return trame;
 }
 
-void sendPulse(int mode) { // falling (1), rising (0)
-  digitalWrite(PIN_OUT, mode);
-  delayMicroseconds(100);
+// void sendPulse(int mode) { // falling (1), rising (0)
+//   digitalWrite(PIN_OUT, mode);
+//   delayMicroseconds(100);
 
-  digitalWrite(PIN_OUT, !mode);
-  delayMicroseconds(100);
-}
+//   digitalWrite(PIN_OUT, !mode);
+//   delayMicroseconds(100);
+// }
 
 void sendZero() {
   digitalWrite(PIN_OUT, LOW);
-  delayMicroseconds(500);
+  delayMicroseconds(HALF_PERIOD);
 
   digitalWrite(PIN_OUT, HIGH);
-  delayMicroseconds(500);
+  delayMicroseconds(HALF_PERIOD);
 }
 
 void sendOne() {
   digitalWrite(PIN_OUT, HIGH);
-  delayMicroseconds(500);
+  delayMicroseconds(HALF_PERIOD);
 
   digitalWrite(PIN_OUT, LOW);
-  delayMicroseconds(500);
+  delayMicroseconds(HALF_PERIOD);
 }
 
 // void sendTrame(uint8_t *trame, int trame_size) {
@@ -103,15 +113,42 @@ void sendOne() {
 /*-------------------- Interrups -------------------*/
 /*--------------------------------------------------*/
 
-void receiveRising() {
+void IRAM_ATTR receiveRising() {
   buffer.push_back(0);
   Serial.println("Rising : 0");
 }
 
-void IRAM_ATTR receiveFalling() {
-  buffer.push_back(1);
-  Serial.println("Falling : 1");
-}
+// void IRAM_ATTR receiveFalling() {
+//   buffer.push_back(1);
+//   Serial.println("Falling : 1");
+// }
+
+// void IRAM_ATTR receivePulse() { 
+//   // Check if rising or falling
+//   int rxVal = digitalRead(PIN_IN);
+//   Serial.printf("\nChange in %d ", rxVal);
+//   // if(checkPeriod) {
+//   //   int diff = timer - lastChangeTime;
+//   //   if((diff >= diff - THRESHOLD_PERIOD) && (diff <= diff + THRESHOLD_PERIOD)) { // one period has pass
+//   //     checkPeriod = false;
+//   //     // read pin et mettre dans buffer selon rising ou falling sachant que on est au 2eme change
+//   //     // buffer.push_back(!rxVal); 
+//   //   }
+//   //   // si plus qu'une periode
+//   //   //    push selon rising et falling
+//   //   //    checkPeriod = true
+//   // }
+
+
+
+  
+
+//   lastChangeTime = timer;
+//   // Add bit to vector
+//   // Notify task
+
+
+// }
 
 /*--------------------------------------------------*/
 /*---------------------- Tasks ---------------------*/
@@ -130,18 +167,12 @@ void TaskSend(void *pvParameters) {
   const TickType_t xDelay = 500;
   while(true) {
     // Serial.println("TaskSend");
-    // sendZero();
+    sendZero();
     // delay(1000);
-    sendOne();
+    // sendOne();
     vTaskDelay(xDelay);
     // delay(1000);
   }
 }
-
-
-
-
-
-
 
 
